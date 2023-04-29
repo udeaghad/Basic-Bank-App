d117d3da9264da59b5e0cb16a3b817441373b96c
udeaghad <dozie.udeagha@gmail.com>
2023-04-29 17:12:35 +0100

    Create test for auths routes/controller

--- src/__test__/authsTest/authsTest.test.ts
+++ src/__test__/authsTest/authsTest.test.ts
@@ -1,4 +1,5 @@
-import request from 'supertest'
+import request from 'supertest';
+
 import Knex from 'knex';
 import app from '../../app';
 
@@ -16,15 +17,116 @@ describe('Accounts', () => {
       name: 'James Brown', 
       email: 'james10@example.com', 
       password: '12345',
-      confirmPassword: '12345', 
-      balance: 25,             
+      confirmPassword: '12345',              
     }
-    const {body} = await request(app).post('/api/v1/auths/account/create').send(newAccunt)
+    const {statusCode, body} = await request(app).post('/api/v1/auths/account/create').send(newAccunt)
 
-    console.log(body)
-
-    // expect(statusCode).toEqual(201)
-    // expect(text).toBe("Company created successfully")
+    expect(statusCode).toEqual(201)
+    expect(body.message).toBe('Account created successfully')
 
   })
+
+  it('Should return error if account already exist', async() => {
+    const newAccunt = { 
+      name: 'James Brown', 
+      email: 'james10@example.com', 
+      password: '12345',
+      confirmPassword: '12345',           
+    }
+      await request(app).post('/api/v1/auths/account/create').send(newAccunt)
+    const {statusCode, body }= await request(app).post('/api/v1/auths/account/create').send(newAccunt)
+
+    expect(statusCode).toEqual(409)
+    expect(body.message).toBe('Account already exist')
+
+  })
+
+  it('Should return error if there is password mismatch', async() => {
+    const newAccunt = { 
+      name: 'James Brown', 
+      email: 'james10@example.com', 
+      password: '12345',
+      confirmPassword: '1234',           
+    }
+      await request(app).post('/api/v1/auths/account/create').send(newAccunt)
+    const {statusCode, body }= await request(app).post('/api/v1/auths/account/create').send(newAccunt)
+
+    expect(statusCode).toEqual(401)
+    expect(body.message).toBe('Password mismatch')
+    expect(body.status).toBe('fail')
+
+  })
+
+  it('Should login user successfully', async() => {
+    //Create user account
+    const newAccunt = { 
+      name: 'James Brown', 
+      email: 'james10@example.com', 
+      password: '12345',
+      confirmPassword: '12345',           
+    }
+      await request(app).post('/api/v1/auths/account/create').send(newAccunt)
+      
+    //login user
+    const userLoginDetails = {
+      email: 'james10@example.com', 
+      password: '12345',
+    }
+    const {statusCode, body }= await request(app).post('/api/v1/auths/account/login').send(userLoginDetails)
+
+    expect(statusCode).toEqual(200)
+    expect(body.message).toBe('Login successful')
+    expect(body.status).toBe('Success')
+    expect(body.data.name).toBe('James Brown')
+    expect(body.data.email).toBe('james10@example.com')
+    expect(body.data.balance).toBe(0)    
+
+  })  
+
+
+  it('Should error if email is incorrect', async() => {
+    //Create user account
+    const newAccunt = { 
+      name: 'James Brown', 
+      email: 'james10@example.com', 
+      password: '12345',
+      confirmPassword: '12345',           
+    }
+      await request(app).post('/api/v1/auths/account/create').send(newAccunt)
+      
+    //login user
+    const userLoginDetails = {
+      email: 'james11@example.com', 
+      password: '12345',
+    }
+    const {statusCode, body }= await request(app).post('/api/v1/auths/account/login').send(userLoginDetails)
+
+    expect(statusCode).toEqual(401)
+    expect(body.message).toBe('Invalid Login Details')
+    expect(body.status).toBe('fail')
+    
+  })  
+
+  it('Should error if password is incorrect', async() => {
+    //Create user account
+    const newAccunt = { 
+      name: 'James Brown', 
+      email: 'james10@example.com', 
+      password: '12345',
+      confirmPassword: '12345',           
+    }
+      await request(app).post('/api/v1/auths/account/create').send(newAccunt)
+      
+    //login user
+    const userLoginDetails = {
+      email: 'james10@example.com', 
+      password: '123456789',
+    }
+    const {statusCode, body }= await request(app).post('/api/v1/auths/account/login').send(userLoginDetails)
+
+    expect(statusCode).toEqual(401)
+    expect(body.message).toBe('Invalid Login Details')
+    expect(body.status).toBe('fail')
+    
+  })  
 })
--- src/controllers/authsController.ts
+++ src/controllers/authsController.ts
@@ -15,7 +15,7 @@ const secretKey: Secret = String(process.env.JWT);
 
 export const createAccount = async(req:Request, res:Response, next:NextFunction) => {
   const { name, email, password, confirmPassword} = req.body;
-  if (password !== confirmPassword) res.status(401).json({status: "fail", message: "Password mismatch"})
+  if (password !== confirmPassword) return res.status(401).json({status: "fail", message: "Password mismatch"})
   
   const accountExist: null | IAccount= await findAccount(email)
   if(accountExist) return res.status(409).json({status: "Success", message: "Account already exist"})
@@ -47,14 +47,14 @@ export const login = async(req: Request, res: Response, next: NextFunction) => {
       const { id, password, ...otherDetails} = accountExist;
       
       const validPassword = await bcrypt.compare(req.body.password, password);
-      if(!validPassword) return res.status(401).json({status: "fail", message: "Invaid Login Details"})
+      if(!validPassword) return res.status(401).json({status: "fail", message: "Invalid Login Details"})
 
       const token = jwt.sign({id}, secretKey)
 
       res
         .cookie("access_token", token, { httpOnly: true, sameSite: "none", secure: true })
         .status(200)
-        .json({id, ...otherDetails})
+        .json({ status: 'Success', message: 'Login successful', data: {id, ...otherDetails}})
     } catch (error) {
       next(error)
     }
